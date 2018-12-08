61a2c688ad603e9dd80c3c1ec6d362a96f27c72e
Matteo Colombo <matte.colo@gmail.com>
2018-12-08 12:07:08 +0100

    add sitemap

---
+++ src/api/v0/sitemap.api.ts
@@ -0,0 +1,96 @@
+import { getCustomRepository } from "typeorm";
+import { Router } from "express";
+
+import { ArticleEntity } from "../../db/entity/article.entity";
+import { ArticleRepository } from "../../db/repository/article.repository";
+import { TutorialRepository } from "../../db/repository/tutorial.repository";
+import { TutorialEntity } from "../../db/entity/tutorial.entity";
+//# we need this because otherwise passport doesn't work
+const router: Router = Router();
+
+const header: String = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
+const websiteBase: string = "https://www.cubingitaly.org/";
+class urlClass {
+    loc: string;
+    lastmod?: string;
+
+    public import(source: Object, base: string) {
+        this.loc = websiteBase + typeBase[base] + source['id'];
+        let temp: Date = source['updateDate'];
+        this.lastmod= temp.toISOString();
+    }
+}
+
+let typeBase = {
+    homepage: "",
+    article: "articoli/",
+    tutorial: "tutorial/",
+    competitions: "competizioni/"
+}
+
+function getArticlesRepo(): ArticleRepository {
+    return getCustomRepository(ArticleRepository);
+}
+
+function getTutorialRepo(): TutorialRepository {
+    return getCustomRepository(TutorialRepository);
+}
+
+function getStaticPages(): urlClass[] {
+    let pages: urlClass[] = [];
+    pages.push(createStaticPage("https://www.cubingitaly.org"));
+    pages.push(createStaticPage("https://www.cubingitaly.org/chi-siamo"));
+    pages.push(createStaticPage("https://www.cubingitaly.org/articoli"));
+    pages.push(createStaticPage("https://www.cubingitaly.org/tutorial"));
+    pages.push(createStaticPage("https://www.cubingitaly.org/competizioni/proposte"));
+    pages.push(createStaticPage("https://www.cubingitaly.org/contatti"));
+    return pages;
+}
+
+function createStaticPage(url: string): urlClass {
+    let temp: urlClass = new urlClass();
+    temp.loc = url;
+    return temp;
+}
+
+router.get("/", async (req, res) => {
+    let pages: urlClass[] = getStaticPages();
+
+    let articles: ArticleEntity[] = await (getArticlesRepo()).getPublicArticles(0, 99999);
+
+    let temp: urlClass = new urlClass();
+
+    for (let a of articles) {
+        temp = new urlClass();
+        temp.import(a, "article");
+        pages.push(temp);
+    }
+
+    let tutorials: TutorialEntity[] = await (getTutorialRepo()).getTutorials();
+
+    for (let t of tutorials) {
+        temp = new urlClass();
+        temp.import(t, "tutorial");
+        pages.push(temp);
+    }
+
+    res.set("Content-Type", "text/xml");
+    let urlencode = {
+        urlencode: pages
+    }
+
+    let xml = header;
+    for (let p of pages) {
+        xml += "<url>";
+        xml += "<loc>" + p.loc + "</loc>";
+        if (p.lastmod) {
+            xml += "<lastmod>" + p.lastmod + "</lastmod>";
+        }
+        xml += "</url>";
+    }
+    xml += "</urlset>";
+
+    res.send(xml);
+});
+
+export { router }
--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,7 @@ import { router as categoryRoutes } from './api/v0/category.api';
 import { router as pageRoutes } from './api/v0/page.api';
 import { router as tutorialRoutes } from './api/v0/tutorial.api';
 import { router as contactRoutes } from './api/v0/contact.api';
+import { router as sitemap } from './api/v0/sitemap.api';
 
 
 const PORT = normalizePort(process.env.PORT || 4300);
@@ -63,6 +64,7 @@ function addRoutes() {
     app.use("/api/v0/pages", pageRoutes);
     app.use("/api/v0/tutorial", tutorialRoutes);
     app.use("/api/v0/contact", contactRoutes);
+    app.use("/sitemap", sitemap);
 }
 
 function addStaticFiles() {
